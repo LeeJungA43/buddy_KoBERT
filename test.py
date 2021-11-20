4ea66d402bcace85b0e9b9048f3b04c42a2dd8d8
LeeJungA43 <82315604+LeeJungA43@users.noreply.github.com>
2021-11-20 19:29:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 files = {"wave": open('./test/buddytest06.wav', 'rb')}
 
 # API 사용. file을 전송
-res1 = requests.post('http://52.231.8.193:5000/test5', files=files)
+res1 = requests.post('<API 주소>/test5', files=files)
 
 f1 = open('./test.wav', 'wb')
 f1.write(res1.content)
